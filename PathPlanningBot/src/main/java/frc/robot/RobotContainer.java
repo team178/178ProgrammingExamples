a14e868d4740eb2192b7ee7092c70cf910d92245
22lejo <22lejo@fpsct.org>
2022-06-07 10:09:19 -0400

    Cleaned up generateRamseteCommand, and made it so paths load on init instead of while the robot runs

--- PathPlanningBot/src/main/java/frc/robot/RobotContainer.java
+++ PathPlanningBot/src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -15,25 +13,18 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.ArcadeDrive;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.ExampleSubsystem;
 import libs.OI.ConsoleController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 
 /**
@@ -70,7 +61,7 @@ public class RobotContainer {
    * 
    * @return A SequentialCommand that sets up and executes a trajectory following Ramsete command
    */
-  private Command generateRamseteCommand(String pathName) {
+  private Command generateRamseteCommand(Trajectory pathweaverTrajectory) {
     var autoVoltageConstraint =
     new DifferentialDriveVoltageConstraint(
       new SimpleMotorFeedforward(
@@ -80,40 +71,9 @@ public class RobotContainer {
       DriveConstants.kDriveKinematics,
       10);
 
-    // Create config for trajectory
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(DriveConstants.kDriveKinematics)
-            // Apply the voltage constraint
-            .addConstraint(autoVoltageConstraint);
-
-    // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            // Pass config
-            config);
-
-    String trajectoryJSON = "output/" + pathName + ".wpilib.json";//replace test with name of path
-    trajectory = new Trajectory();
-    try {
-      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-    } catch (IOException ex) {
-      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
-    }
-
     RamseteCommand ramseteCommand =
         new RamseteCommand(
-            exampleTrajectory,
+          pathweaverTrajectory, //Can be replaced by exampleTrajectory if so wished
             m_drivetrain::getPose,
             new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
             new SimpleMotorFeedforward(
@@ -128,7 +88,7 @@ public class RobotContainer {
             m_drivetrain::tankDriveVolts,
             m_drivetrain);
 
-    m_drivetrain.resetOdometry(trajectory.getInitialPose());
+    m_drivetrain.resetOdometry(pathweaverTrajectory.getInitialPose());
 
     // Run path following command, then stop at the end.
     return ramseteCommand.andThen(() -> m_drivetrain.tankDriveVolts(0, 0));
@@ -144,9 +104,30 @@ public class RobotContainer {
 
   private void configureSmartDashboard() {
     // Setup SmartDashboard options
-    m_chooser.addOption("Ramsete Trajectory", generateRamseteCommand("test"));
+    m_chooser.addOption("Ramsete Trajectory", generateRamseteCommand(
+      getPathWeaverTrajctory("exampleTrajectory")));
 
     SmartDashboard.putData("Autnomous Routine", m_chooser);
+  }
+
+  /**
+   * Used to easily load paths during initialization of the Robot, which is done then because: 
+   * "Loading a PathWeaver JSON from file in Java can take more than one loop iteration so it is highly recommended that the robot load these paths on startup".
+   * 
+   * @param pathName
+   * @return
+   */
+  public Trajectory getPathWeaverTrajctory(String pathName){
+    String trajectoryJSON = "output/" + pathName + ".wpilib.json";//replace test with name of path
+    trajectory = new Trajectory();
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    }
+
+    return trajectory;
   }
 
   /**
